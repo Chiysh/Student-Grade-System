14bca041b9023bff895957a53b6e2fe3f0fa07de
Chiysh <151706337+Chiysh@users.noreply.github.com>
2023-12-22 18:36:18 +0800

    Update AdminPanelView.java

--- AdminPanelView.java
+++ AdminPanelView.java
@@ -3,22 +3,33 @@ package project;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AdminPanelView {
-    public static void showAdminPanel() {
-        JFrame adminFrame = new JFrame("Admin Panel");
+public class AdminPanelView extends JPanel {
+    public AdminPanelView() {
+        setLayout(null); 
 
         JPanel adminPanel = new JPanel();
+        adminPanel.setLayout(null); 
         adminPanel.setBounds(0, 0, 400, 200);
 
         JLabel welcomeLabel = new JLabel("Welcome, Admin!");
         welcomeLabel.setFont(new Font("Inter", Font.BOLD, 24));
+        welcomeLabel.setBounds(50, 50, 300, 50);
         adminPanel.add(welcomeLabel);
 
-        adminFrame.setSize(400, 200);
-        adminFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        adminFrame.setLayout(null);
-        adminFrame.setVisible(true);
-        adminFrame.add(adminPanel);
+        JButton backButton = new JButton("Back to Home");
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainFrame.showHomePanel();
+            }
+        });
+
+        backButton.setBounds(600, 280, 115, 40); // Adjust these values as needed
+
+        add(adminPanel);
+        add(backButton);
     }
 }
